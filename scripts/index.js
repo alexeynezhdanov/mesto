13da15db4de92428a4988d4e8724ba348c56dc5e
alexeynezhdanov <alexeynezhdanov@yandex.ru>
2022-04-10 15:15:57 +0300

    Проектная работа 5 с исправлениями

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,23 @@
-let editButton = document.querySelector('.profile__edit-button');
-let addButton = document.querySelector('.profile__add-button');
-let closeButtonPopupProfile = document.querySelector('.popup__close-button-profile');
-let closeButtonPopupCard = document.querySelector('.popup__close-button-card');
-let closeButtonPopupView = document.querySelector('.popup__close-button-view');
-let popupProfile = document.querySelector('.popup_profile');
-let popupCard = document.querySelector('.popup_card');
-let popupView = document.querySelector('.popup_view');
-let textName = document.getElementById('textName');
-let aboutMe = document.getElementById('aboutMe');
-let cardName = document.getElementById('cardName');
-let cardLink = document.getElementById('cardLink');
-let profileName = document.querySelector('.profile__name');
-let profileAboutMe = document.querySelector('.profile__about-me');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const closeButtonPopupProfile = document.querySelector('.popup__close-button-profile');
+const closeButtonPopupCard = document.querySelector('.popup__close-button-card');
+const closeButtonPopupView = document.querySelector('.popup__close-button-view');
+const saveButtonCard = document.querySelector('form[name="card-info"]');
+const saveButtonProfile = document.querySelector('form[name="profile-info"]');
+const popupProfile = document.querySelector('.popup_profile');
+const popupCard = document.querySelector('.popup_card');
+const popupView = document.querySelector('.popup_view');
+const textName = document.getElementById('textName');
+const aboutMe = document.getElementById('aboutMe');
+const cardName = document.getElementById('cardName');
+const cardLink = document.getElementById('cardLink');
+const profileName = document.querySelector('.profile__name');
+const profileAboutMe = document.querySelector('.profile__about-me');
+const elements = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#card').content;;
+const popupViewImage = document.querySelector('.popup__image');
+const popupViewLabel = document.querySelector('.popup__label');
 
 /* Исходный массив карточек */
 const initialCards = [
@@ -41,94 +47,110 @@ const initialCards = [
   }
 ];
 
-/* Добавление карточки */
-function addCard (name, link) {
-  const elements = document.querySelector('.elements');
-  const cardTemplate = document.querySelector('#card').content;
+function createCard(name, link) {
   const cardElement = cardTemplate.querySelector('.elements__element').cloneNode(true);
-  cardElement.querySelector('.elements__name').textContent = name;
-  cardElement.querySelector('.elements__photo').src = link;
+  const photo = cardElement.querySelector('.elements__photo');
+  const caption = cardElement.querySelector('.elements__caption');
+  caption.textContent = name;
+  photo.src = link;
+  photo.alt = name;
+  openPopupView(photo, caption);
+  likes(cardElement);
+  deleteCard(cardElement);
+  return cardElement;
+};
 
-/* Лайк */
-  cardElement.querySelector('.elements__like').addEventListener('click', function (evt){
-    console.log('Мы кликнули по элементу Like');
-    evt.target.classList.toggle('element__like_active');
-  });
-
-/* Корзина */
-  const deleteButton = cardElement.querySelector('.elements__basket');
-  deleteButton.addEventListener('click', function () {
-  console.log('Мы кликнули по элементу Basket');
-  const listItem = deleteButton.closest('.elements__element');
-  listItem.remove();
-  });
-
-/* Попап View */
-  cardElement.querySelector('.elements__photo').addEventListener('click', function () {
-  console.log('Мы кликнули по элементу photo');
-  popupView.classList.add('popup_opened');
-  document.querySelector('.popup__image').src = cardElement.querySelector('.elements__photo').src;
-  document.querySelector('.popup__label').textContent = cardElement.querySelector('.elements__name').textContent;
-  });
-
+function addCard(name, link) {
+  const cardElement = createCard(name, link);
   elements.prepend(cardElement);
 };
 
-function showInitialCards () {
-  initialCards.forEach((i) => {
-addCard(i.name, i.link);
-});
+function showInitialCards() {
+  initialCards.forEach((item) => {
+    addCard(item.name, item.link);
+  });
 };
 showInitialCards();
 
-/* Попап профиля */
-function clickOpenPopupProfile () {
-  console.log('Мы кликнули по элементу openPopupProfile');
+/* Попап Profile */
+function openPopupProfile() {
   textName.value = profileName.textContent;
   aboutMe.value = profileAboutMe.textContent;
-  popupProfile.classList.add('popup_opened');
+  openPopup(popupProfile);
 };
 
-function clickCloseButtonProfile () {
-  console.log('Мы кликнули по элементу Close');
-  popupProfile.classList.remove('popup_opened');
-};
-
-function addProfile (popupProfile) {
-  popupProfile.preventDefault();
+function saveProfile(event) {
+  event.preventDefault();
   profileName.textContent = textName.value;
   profileAboutMe.textContent = aboutMe.value;
-  clickCloseButtonProfile ();
+  closePopupProfile();
 };
 
-/* Попап карточки */
-function clickOpenPopupCard () {
-  console.log('Мы кликнули по элементу openPopupCard');
-  popupCard.classList.add('popup_opened');
+function closePopupProfile() {
+  clickClose(popupProfile);
 };
 
-function clickCloseButtonCard () {
-  console.log('Мы кликнули по элементу Close');
-  popupCard.classList.remove('popup_opened');
+/* Попап Card */
+function openPopupCard() {
+  openPopup(popupCard);
 };
 
-/* Закрытие попапа просмотра изображений */
-function clickCloseButtonView () {
-  console.log('Мы кликнули по элементу Close');
-  popupView.classList.remove('popup_opened');
+function saveCard(event) {
+  event.preventDefault();
+  addCard(cardName.value, cardLink.value);
+  addCard.reset;
+  closePopupCard();
+};
+
+function closePopupCard() {
+  clickClose(popupCard);
+};
+
+/* Попап View */
+function openPopupView(photo, label) {
+  photo.addEventListener('click', function () {
+    openPopup(popupView);
+    popupViewImage.src = photo.src;
+    popupViewImage.alt = label.textContent;
+    popupViewLabel.textContent = label.textContent;
+  });
+};
+
+function closePopupView() {
+  clickClose(popupView);
+};
+
+/* Открытие попапов */
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+};
+
+/* Закрытие попапов */
+function clickClose(popup) {
+  popup.classList.remove('popup_opened');
+};
+
+/* Лайки */
+function likes(cardElement) {
+  cardElement.querySelector('.elements__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
+  });
+};
+
+/* Корзина */
+function deleteCard(cardElement) {
+  const deleteButton = cardElement.querySelector('.elements__basket');
+  deleteButton.addEventListener('click', function () {
+    const listItem = deleteButton.closest('.elements__element');
+    listItem.remove();
+  });
 };
 
 /* Слушатели */
-editButton.addEventListener('click', clickOpenPopupProfile);
-addButton.addEventListener('click', clickOpenPopupCard);
-closeButtonPopupProfile.addEventListener('click', clickCloseButtonProfile);
-closeButtonPopupCard.addEventListener('click', clickCloseButtonCard);
-closeButtonPopupView.addEventListener('click', clickCloseButtonView);
-document.querySelector('form[name="profile-info"]').addEventListener('submit', addProfile);
-document.querySelector('form[name="card-info"]').addEventListener('submit', function (popupCard) {
-  popupCard.preventDefault();
-  addCard(cardName.value, cardLink.value);
-  cardName.value = '';
-  cardLink.value = '';
-  clickCloseButtonCard ();
-});
+editButton.addEventListener('click', openPopupProfile);
+addButton.addEventListener('click', openPopupCard);
+closeButtonPopupProfile.addEventListener('click', closePopupProfile);
+closeButtonPopupCard.addEventListener('click', closePopupCard);
+closeButtonPopupView.addEventListener('click', closePopupView);
+saveButtonProfile.addEventListener('submit', saveProfile);
+saveButtonCard.addEventListener('submit', saveCard);
